1f80ce1059a1fe28cc3b0497c02e2cba8e391c1a
Mirza Asfandyar Baig <mirza.business15@gmail.com>
2025-05-19 23:09:01 +0500

    Learned basics

---
+++ initial.py
@@ -0,0 +1,25 @@
+print("Hello");
+
+# age_str = "25"
+# age = int(age_str)
+# print("you are " + str(age) + " years old")
+
+# data types and typecasting basics
+
+age_str = "25"
+age = int(age_str)
+print(age+5)
+
+score = 99
+score_str = str(score)
+print("Your score is: " + score_str)
+
+pi = 3.14234
+rounded = int(pi)
+print(rounded)
+
+#turning anything into bool:
+print(bool(0))
+print(bool("Hello"))
+print(bool(""))
+
