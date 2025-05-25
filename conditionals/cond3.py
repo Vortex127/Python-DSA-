6eb2c4519852ba6bcd7db5b863edf91a1d0edee7
Vortex127 <mirza.business15@gmail.com>
2025-05-26 00:17:57 +0500

    Logic++

---
+++ conditionals/cond3.py
@@ -0,0 +1,14 @@
+#3 Grade Calculator
+# Assign a letter grade based on a student's score: A(90-100), B(80-89), C(70-79), D(60-69), F(below 60).
+
+marks = int(input("Enter your marks: "));
+if marks >= 90 and marks <= 100:
+    print("A")
+elif marks >= 80 and marks < 90:
+    print("B")    
+elif marks >= 70 and marks < 80:
+    print("C")
+elif marks >= 60 and marks < 70:
+    print("D")
+elif marks < 60:
+    print("F")
