45977018ee49d76531d2677bda14e6cf46891c62
Vortex127 <mirza.business15@gmail.com>
2025-05-26 00:21:34 +0500

    refactored

--- conditionals/cond3.py
+++ conditionals/cond3.py
@@ -2,13 +2,13 @@
 # Assign a letter grade based on a student's score: A(90-100), B(80-89), C(70-79), D(60-69), F(below 60).
 
 marks = int(input("Enter your marks: "));
-if marks >= 90 and marks <= 100:
+if marks >= 90:
     print("A")
-elif marks >= 80 and marks < 90:
+elif marks >= 80:
     print("B")    
-elif marks >= 70 and marks < 80:
+elif marks >= 70:
     print("C")
-elif marks >= 60 and marks < 70:
+elif marks >= 60:
     print("D")
 elif marks < 60:
     print("F")
